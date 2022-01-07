1b003f625758a1831e6b8e34e64748abef5f8ac6
ade18121003@gmail.com <ade18121003@gmail.com>
2022-01-07 16:02:37 +0700

    Tugasmuko

---
+++ database/migrations/2022_01_07_085010_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id()->unsigned();
+            $table->char('name', 191);
+            $table->char('descriptions', 191);
+            $table->char('stock', 191);
+            $table->char('price', 191);
+            $table->Integer('category_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
